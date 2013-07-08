aa1bdd9185977efd797b45d6e9aec8874aa5766f
Patrick Wolleb <patrick.wolleb@shmedia.co.uk>
2013-07-08 14:56:19 +0100

    Added build version to footer

--- src/app/controllers/pages.js
+++ src/app/controllers/pages.js
@@ -3,7 +3,10 @@ module.exports = function(config) {
 	var api = {};
 
 	api.index = function(req, res) {
-		res.render('index', {layout:false,locals:{version:config.version, title: 'SH MEDIA', description:'Web App Template', fbAppID: config.facebook.clientID}});
+		res.render('index', {layout:false,locals:{
+			version:config.version, 
+			title: 'SH MEDIA', description:'Web App Template', 
+			fbAppID: config.facebook.clientID}});
 	}
 
 	api.canvas = function(req, res) {
